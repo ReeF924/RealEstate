77a3e0f586e38b7a97cb467a398af27159afd0c1
ReeF <vesecky.lukas90@gmail.com>
2023-12-11 21:46:33 +0100

    added editUsers

--- RealEstate/Controllers/LoginController.cs
+++ RealEstate/Controllers/LoginController.cs
@@ -7,6 +7,7 @@ using System.Text.Json;
 using RealEstate.Services;
 using RealEstate.Models;
 using Microsoft.AspNetCore.Identity;
+using Org.BouncyCastle.Asn1.Mozilla;
 
 namespace RealEstate.Controllers
 {
@@ -54,14 +55,26 @@ namespace RealEstate.Controllers
         }
 
         [HttpGet]
-        public IActionResult SignUp(User? user = null)
+        public IActionResult SignUp(int? idUser = null)
         {
-            return View(user ?? new User());
+            User user;
+            if(idUser != null)
+            {
+                user = this._context.Users!.Where(user => user.Id == idUser).First();
+                user.Password = "";
+            }
+            else
+                user = new User();
+
+            this.ViewBag.IdUser = idUser;   
+            this.ViewBag.Type = idUser == null ? "Create" : "Edit";
+            return View(user);
         }
 
         [HttpPost]
         public IActionResult CreateUser(User input)
         {
+            //validace
             input.Password = BCrypt.Net.BCrypt.HashPassword(input.Password);
 
             this._context.Users!.Add(input);
@@ -71,6 +84,26 @@ namespace RealEstate.Controllers
 
             return RedirectToAction("Index", "EstateOffers");
         }
+
+        [HttpPost]
+        public IActionResult EditUser(User input, int idUser)
+        {
+            //validace
+            User user = this._context.Users!.Where(user => user.Id == idUser).First();
+            user.Username = input.Username;
+            user.Email = input.Email;
+            user.Password = !String.IsNullOrEmpty(input.Password) ? BCrypt.Net.BCrypt.HashPassword(input.Password) : user.Password;
+            user.Name = input.Name;
+            user.Surname = input.Surname;
+            user.PhoneNumber = input.PhoneNumber;
+
+
+            this._context.SaveChanges();
+            this.HttpContext.Session.SetString("User", JsonSerializer.Serialize(user));
+
+            return RedirectToAction("Index", "EstateOffers");
+        }
+
         public IActionResult Logout()
         {
             this.HttpContext.Session.Remove("User");
